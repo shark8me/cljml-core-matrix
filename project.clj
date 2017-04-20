034fc6f01ef887058124b6b001f8ab846f807d95
Kiran Karkera <kiran.karkera@gmail.com>
2017-04-20 17:45:15 +0800

    ignore changes

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :test-paths  ["src/" "test/" ]
+  :plugins  [[lein-codox "0.10.3"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.mikera/core.matrix "0.57.0"]
                  [com.rpl/specter "1.0.1"]
